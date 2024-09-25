ee9837537101bdbf40732e3fbc55bd5e010bfc14
Mohamed Ibrahim <78230140+MohamedIbrahim222@users.noreply.github.com>
2024-09-25 10:50:17 +0300

    initial commit

--- src/admin/app.example.js
+++ src/admin/app.example.js
@@ -1,32 +1,88 @@
+const logo = '[url=https://ibb.co/K9p34F9][img]https://i.ibb.co/JzwM65z/customcolor-icon-transparent-background.png[/img][/url]';
+
 const config = {
-  locales: [
-    // 'ar',
-    // 'fr',
-    // 'cs',
-    // 'de',
-    // 'dk',
-    // 'es',
-    // 'he',
-    // 'id',
-    // 'it',
-    // 'ja',
-    // 'ko',
-    // 'ms',
-    // 'nl',
-    // 'no',
-    // 'pl',
-    // 'pt-BR',
-    // 'pt',
-    // 'ru',
-    // 'sk',
-    // 'sv',
-    // 'th',
-    // 'tr',
-    // 'uk',
-    // 'vi',
-    // 'zh-Hans',
-    // 'zh',
-  ],
+  // Replace the Strapi logo in auth (login) views
+  auth: {
+    logo,
+  },
+  // Replace the favicon
+  head: {
+    favicon: logo,
+  },
+  // Replace the Strapi logo in the main navigation
+  menu: {
+    logo,
+  },
+  // Extend the translations
+  translations: {
+    en: {
+      // Left Menu
+      "app.components.LeftMenu.navbrand.title": "Aoun Dashboard",
+      "app.components.LeftMenu.navbrand.workplace": "Testing",
+      "app.components.LeftMenu.general": "General",
+      "app.components.LeftMenu.plugins": "Plugins",
+      "app.components.LeftMenu.settings": "Settings",
+
+      // Header
+      "app.components.Header.profile": "Profile",
+      "app.components.Header.logout": "Log out",
+
+      // HomePage
+      "app.components.HomePage.welcome": "Welcome to your Dashboard",
+      "app.components.HomePage.welcome.again": "Welcome back",
+      "app.components.HomePage.cta": "Get Started",
+
+      // ContentManager
+      "app.components.ContentManager.title": "Content Manager",
+      "app.components.ContentManager.create": "Create new entry",
+      "app.components.ContentManager.edit": "Edit entry",
+
+      // Auth
+      "Auth.form.welcome.title": "Welcome to jasara",
+      "Auth.form.welcome.subtitle": "Login to your account",
+      "Auth.form.button.login": "Log in",
+      "Auth.form.button.forgot-password": "Forgot password?",
+
+      // Settings
+      "Settings.profile.form.section.experience.interfaceLanguageHelp":
+        "Preference changes will apply only to you.",
+    },
+    ar: {
+      // Left Menu
+      "app.components.LeftMenu.navbrand.title": "لوحة تحكم جساره",
+      "app.components.LeftMenu.navbrand.workplace": "جساره",
+      "app.components.LeftMenu.general": "عام",
+      "app.components.LeftMenu.plugins": "الإضافات",
+      "app.components.LeftMenu.settings": "الإعدادات",
+
+      // Header
+      "app.components.Header.profile": "الملف الشخصي",
+      "app.components.Header.logout": "تسجيل الخروج",
+
+      // HomePage
+      "app.components.HomePage.welcome": "مرحبًا بك في لوحة تحكم جساره",
+      "app.components.HomePage.welcome.again": "مرحبًا بعودتك",
+      "app.components.HomePage.cta": "ابدأ الآن",
+
+      // ContentManager
+      "app.components.ContentManager.title": "إدارة المحتوى",
+      "app.components.ContentManager.create": "إنشاء مدخل جديد",
+      "app.components.ContentManager.edit": "تعديل المدخل",
+
+      // Auth
+      "Auth.form.welcome.title": "أهلا بيك في جساره",
+      "Auth.form.welcome.subtitle": "تسجيل الدخول إلى حسابك",
+      "Auth.form.button.login": "تسجيل الدخول",
+      "Auth.form.button.forgot-password": "نسيت كلمة المرور؟",
+
+      // Settings
+      "Settings.profile.form.section.experience.interfaceLanguageHelp": "ستنطبق التغييرات في التفضيلات عليك فقط.",
+    },
+  },
+  // Disable video tutorials
+  tutorials: false,
+  // Disable notifications about new Strapi releases
+  notifications: { releases: false },
 };
 
 const bootstrap = (app) => {
